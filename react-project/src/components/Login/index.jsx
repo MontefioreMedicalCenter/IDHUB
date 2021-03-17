96639257e19bd705270064a67841980f7a890fdd
amitgouda <amit@flexicious.com>
2021-03-17 23:48:25 +0530

    - Initial commit for login screen

---
+++ react-project/src/components/Login/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const Login = () => {
+    return <div className='login-root-container' >
+        <div>
+            login screen
+    </div>
+    </div>
+}
+
+export default Login
