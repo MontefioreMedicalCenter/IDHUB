c1e33375406a9fb332436d2a29c8f9e99f96f457
amit <amit@flexicious.com>
2021-03-18 00:27:53 +0530

    - Initial commit for login screen.

--- react-project/src/components/Login/index.jsx
+++ react-project/src/components/Login/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import TextFeildComponent from '../../shared/components/TextFeildComponent'
 import './styles.scss'
 
 const Login = () => {
     return <div className='login-root-container' >
         <div>
-            login screen
-    </div>
+            <TextFeildComponent /><br />
+            <TextFeildComponent />
+        </div>
     </div>
 }
 
---
+++ react-project/src/shared/components/TextFeildComponent/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+
+const TextFeildComponent = () => {
+    return (
+        <React.Fragment>
+            <TextField id="user-name" label="user-name" type="text" variant="outlined" />
+        </React.Fragment>
+    );
+}
+
+export default TextFeildComponent
