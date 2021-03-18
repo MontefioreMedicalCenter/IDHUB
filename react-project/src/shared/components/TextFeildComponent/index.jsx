ef91b19e8e6fbc377c9ff51a719a3056e8f8a7fa
amit <amit@flexicious.com>
2021-03-18 20:57:46 +0530

    LogininScreenUI

--- react-project/src/shared/components/TextFeildComponent/index.jsx
+++ react-project/src/shared/components/TextFeildComponent/index.jsx
@@ -4,7 +4,12 @@ import TextField from '@material-ui/core/TextField';
 const TextFeildComponent = (props) => {
     return (
         <React.Fragment>
-            <TextField id={props.id} label={props.label} type={props.type} variant="outlined" />
+            <TextField
+                id={props.id}
+                label={props.label}
+                type={props.type}
+            />
+            {/* variant="outlined"  */}
         </React.Fragment>
     );
 }
