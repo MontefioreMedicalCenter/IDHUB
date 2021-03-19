ca893689d772effb1a90c001d8f816a21cc6785e
amit <amit@flexicious.com>
2021-03-19 23:04:46 +0530

    - latest build

---
+++ react-project/build/precache-manifest.fd56862223e60c8b6fefc5e87cc17a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7780a58fc56f2bd55c3f",
+    "url": "/static/js/runtime~main.7780a58f.js"
+  },
+  {
+    "revision": "b6a8f0efc3ac31f09c4a",
+    "url": "/static/js/main.b6a8f0ef.chunk.js"
+  },
+  {
+    "revision": "d7c7db9c3a8bc0c42e77",
+    "url": "/static/js/2.d7c7db9c.chunk.js"
+  },
+  {
+    "revision": "8c5b544e4ad429dffe10",
+    "url": "/static/js/1.8c5b544e.chunk.js"
+  },
+  {
+    "revision": "b6a8f0efc3ac31f09c4a",
+    "url": "/static/css/main.afe89f14.chunk.css"
+  },
+  {
+    "revision": "9f3bbb1e6622e7af01b48f11bfa2865c",
+    "url": "/index.html"
+  }
+];
