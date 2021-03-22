71c617af60bb4660bd442e1a1c5d487766037baa
shyam <shyam@flexicious.com>
2021-03-22 21:40:55 +0530

    - Implemented functionality of dynamic basename with browser router.

--- react-project/request-response/dummy-backend/server.js
+++ react-project/request-response/dummy-backend/server.js
@@ -16,7 +16,7 @@ app.post('/IdentityHub/api/authenticationsvc/authenticateUser', (req, res) => {
 
 app.use(require('express').static(path.join(__dirname, 'build')));
 
-app.get('/IdentityHub', function(req,res) {
+app.get('/', function(req,res) {
 		res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
