e09157bb833126e04563e4906c7e8c10d0c64801
mmishra1008 <77509761+mmishra1008@users.noreply.github.com>
2021-04-12 16:40:56 -0400

    Add files via upload

---
+++ react-project/request-response/dummy-backend/response/saveWorkGroupResponse.js
@@ -0,0 +1,80 @@
+module.exports ={
+    "additionalComments": "I have more comments again and again",
+    "createDate": "2021-01-28T15:13:43Z[UTC]",
+    "createdBy": "Job16523",
+    "requesterUser": {
+        "createDate": "2018-03-26T04:00:00Z[UTC]",
+        "createdBy": "system",
+        "roleMap": [
+            {
+                "accessActiveFlag": 1,
+                "createdBy": "system",
+                "id": {
+                    "roleId": "Requestor",
+                    "userId": "mmishra"
+                },
+                "updatedBy": "system"
+            }
+        ],
+        "updateDate": "2018-03-26T04:00:00Z[UTC]",
+        "updatedBy": "system",
+        "userActiveFlag": 1,
+        "userEmail": "mmishra@montefiore.org",
+        "userFirstName": "Mittul",
+        "userId": "mmishra",
+        "userLastName": "Mishra",
+        "userPhone": "914-457-6018"
+    },
+    "requesterUserId": "mmishra",
+    "reviewerComments": "Screen Shot",
+    "reviewerUserId": "dpalermo",
+    "submitDate": "2021-02-16T19:29:14Z[UTC]",
+    "updateDate": "2021-02-16T19:29:03Z[UTC]",
+    "updateRequestor": true,
+    "updatedBy": "dpalermo",
+    "workLists": [
+        {
+            "additionalComments": "More sample data",
+            "affiliateEmailRequest": "N",
+            "campusCode": "Other",
+            "companyCode": "ABC Corp",
+            "createDate": "2021-01-28T15:13:43Z[UTC]",
+            "createdBy": "Job16523",
+            "dateOfBirth": "1985-03-24T05:00:00Z[UTC]",
+            "department": "Montefiore IT",
+            "documentCount": 0,
+            "employeeSubGroup": "Non-clinical Consultant",
+            "endDate": "2021-06-30T04:00:00Z[UTC]",
+            "epcsHardTokenRequest": "N",
+            "epfRequest": "N",
+            "epicRequest": "N",
+            "firstName": "Sheila",
+            "gender": "F",
+            "homeDriveRequest": "N",
+            "id": {
+                "worklistId": "wk.00011573",
+                "worklistSeqNum": 1
+            },
+            "lastName": "Samson",
+            "managerEmail": "mgrname@montefiore.org",
+            "managerPhone": "914-123-4567",
+            "managerSourceUniqueId": "123456",
+            "middleNameOrInitial": "F",
+            "mmcEmailRequest": "N",
+            "noSSN": "N",
+            "nonMonteEmail": "ss@aol.com",
+            "reviewerComments": "Screen Shot",
+            "sourceSystem": "NonEmployee",
+            "sourceUniqueId": "C633627",
+            "ssn": "4321",
+            "startDate": "2021-02-15T05:00:00Z[UTC]",
+            "statusCode": "A",
+            "title": "Consultant Coder",
+            "updateDate": "2021-02-16T19:29:03Z[UTC]",
+            "updatedBy": "beweeks",
+            "worklistStatus": "Rejected"
+        }
+    ],
+    "worklistId": "wk.00011573",
+    "worklistStatus": "Rejected"
+}
