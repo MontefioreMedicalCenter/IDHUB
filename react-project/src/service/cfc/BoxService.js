9190b8f30de04bf37cfca3e7fe9bbe9afd8b3c55
flexicious <flexicious>
2021-04-22 19:59:27 -0400

    fix box service

--- react-project/src/service/cfc/BoxService.js
+++ react-project/src/service/cfc/BoxService.js
@@ -8,6 +8,7 @@ export default class BoxService extends ServiceProxyBase {
 
 	sendDocumentsToBox(workListId, resultHandler, faultHandler) {
 		if (typeof faultHandler == 'undefined') faultHandler = (msg)=>{
+			if(msg.error)msg=JSON.stringify(msg.error);
 			if(msg.indexOf("The process cannot access the file because it is being used by another process") < 0){
 				alert(msg)
 			}else{
@@ -15,15 +16,15 @@ export default class BoxService extends ServiceProxyBase {
 			}
 		}
 		if (typeof resultHandler == 'undefined') resultHandler = ()=>{alert("Files Transferred to Box Sucessfully!")}
-		
 		return this.callServiceMethod(
-			'get',
-			'IdentityHub/api/boxsvc/uploadFile',
-			null,
+			'post',
+			'IdentityHub/api/boxsvc/sendDocumentsToBox',
+			workListId,
 			null,
 			resultHandler,
-			faultHandler
-		)
+			faultHandler,
+			'form'
+		);
 	}
 }
 
