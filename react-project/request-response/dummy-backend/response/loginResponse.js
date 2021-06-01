0838e6888d8126673de215a65c208efc69c50594
mmishra1008 <77509761+mmishra1008@users.noreply.github.com>
2021-06-01 09:38:14 -0400

    Added more fields

--- react-project/request-response/dummy-backend/response/loginResponse.js
+++ react-project/request-response/dummy-backend/response/loginResponse.js
@@ -5,20 +5,27 @@ module.exports = {
     "roleMap": [
         {
             "accessActiveFlag": 1,
+            "createDate": "2018-04-03 04:00:00",
             "createdBy": "system",
             "id": {
                 "roleId": "Admin",
                 "userId": "mmishra"
             },
+            "updateDate": "2018-04-03 04:00:00",
             "updatedBy": "system"
         }
     ],
     "updateDate": "2018-03-26 04:00:00",
     "updatedBy": "system",
     "userActiveFlag": 1,
+    "userAddress1": "3 Odell Plaza",
+    "userAddress2": null,
+    "userCity": "Yonkers",
     "userEmail": "mmishra@montefiore.org",
     "userFirstName": "Mittul",
     "userId": "mmishra",
     "userLastName": "Mishra",
-    "userPhone": "914-457-6018"
+    "userPhone": "914-457-6018",
+    "userState": "NY",
+    "userZip": "10701"
 }
