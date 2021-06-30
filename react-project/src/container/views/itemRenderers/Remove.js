1a0236bc9b9cc91b20ba7ba1d1b5c5162927413d
Shyam <shyam@flexicious.com>
2021-06-30 20:46:12 +0530

    -- Fixed - Issue # 137 - On Requestor Worklist, Delete icon is no longer appearing for any requests (individual or grouped) -- Fixed - Issue # 159 - On Search Request, delete icon appears immediately after clicking on current request tab (while processed requests are still displayed in the grid. It's possible for user to click the delete icon for a processed request. Confirm Delete message for the Processed request is displayed. (did not click confirm since testing was in QA and CEC testers are still logging in)

--- react-project/src/container/views/itemRenderers/Remove.js
+++ react-project/src/container/views/itemRenderers/Remove.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
-import { useSelector } from 'react-redux'
+// import { useSelector } from 'react-redux'
 
 const Remove = props => {
-	const tabValue = useSelector(state => state.workListState.tabValue)
+	// const tabValue = useSelector(state => state.workListState.tabValue)
 	const onDelete = () => {
 		props.column.handleDelete(props)
 	}
 
-	if (props.cell.rowInfo && props.cell.rowInfo.getIsDataRow() && tabValue === 0) {
+	if (props.cell.rowInfo && props.cell.rowInfo.getIsDataRow() && props.row.getData().worklistStatus !== "Processed") {
 		return (
 			<Button onClick={onDelete} style={{height:"100%"}}>
 				<DeleteIcon fontSize="small" />
